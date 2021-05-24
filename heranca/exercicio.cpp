d2e8bc81eadc055032eed549249c8fc7c576fe26
mtdecarvalho <matheusdecarvalhorr@gmail.com>
2021-05-24 13:50:08 -0300

    update, prototipo

--- heranca/exercicio.cpp
+++ heranca/exercicio.cpp
@@ -4,66 +4,53 @@ using namespace std;
 
 class Disciplina
 {
-    
+    private:
+        string nome;
+        int codigo, creditos, semestre, ano;
+        double nota1, nota2;
     public:
-        virtual void ler()              {                       }
-        virtual void calcularMedia()    {                       }
-        virtual void alterar()          {                       }
-        virtual string getTipo()        { return "Tipo";        }
-        virtual int getCodigo()         { return 0;             }
-        virtual string getNome()        { return "Disciplina";  }
-        virtual int getCreditos()       { return 0;             }
-        virtual double getMedia()       { return 0;             }
-        virtual double getNota()        { return 0;             }
+        double getNota1()                       { return nota1;         }
+        double getNota2()                       { return nota2;         }
+        virtual void ler();
+        virtual void calcularMedia()            {                       }
+        virtual string getTipo()                { return "Tipo";        }
+        virtual int getCodigo()                 { return codigo;        }
+        virtual string getNome()                { return nome;          }   
+        virtual int getCreditos()               { return creditos;      }
+        virtual double getMedia()               { return 0;             }
+        virtual double getNotaApresentacao()    { return 0;             }
 };
+
+void Disciplina::ler()
+{
+    cout << "Codigo -> "; cin >> codigo;
+    cout << "Nome -> "; cin.ignore(); getline(cin, nome);
+    cout << "Creditos -> "; cin >> codigo;
+    cout << "Semestre -> "; cin >> semestre;
+    cout << "Ano -> "; cin >> ano;
+    cout << "Nota 1 -> "; cin >> nota1;
+    cout << "Nota 2 -> "; cin >> nota2;
+}
 
 class Tipo1 : public Disciplina 
 {
     private:
-        string nome;
-        int codigo, creditos, semestre, ano;
-        double nota1, nota2, media;
+        double media;
     public:
-        void ler();
-        void calcularMedia()    { media = (nota1+nota2)/2;  }
-        string getTipo()        { return "Tipo1";           }
-        int getCodigo()         { return codigo;            }
-        string getNome()        { return nome;              }
-        int getCreditos()       { return creditos;          }
-        double getMedia()       { return media;             }
+        void calcularMedia()     { media = (getNota1()+getNota2())/2;   }
+        double getMedia()        { return media;                        }
+        string getTipo()         { return "Tipo1";                      }
 };
-
-void Tipo1::ler()
-{
-    cin >> codigo; cin.ignore();
-    getline(cin, nome);
-    cin >> creditos >> semestre >> ano >> nota1 >> nota2;
-    calcularMedia();
-}
 
 class Tipo2 : public Disciplina
 {
     private:
-        string nome;
-        int codigo, creditos, semestre, ano;
-        double nota1, nota2, media;
+        double media;
     public:
-        void ler();
-        void calcularMedia()    { media = (nota1+2*nota2)/3;    }
-        string getTipo()        { return "Tipo2";               }
-        int getCodigo()         { return codigo;                }
-        string getNome()        { return nome;                  }
-        int getCreditos()       { return creditos;              }
-        double getMedia()       { return media;                 }
+        void calcularMedia()     { media = (getNota1()+2*getNota2())/3;     }
+        double getMedia()        { return media;                            }
+        string getTipo()         { return "Tipo2";                          }
 };
-
-void Tipo2::ler()
-{
-    cin >> codigo; cin.ignore();
-    getline(cin, nome);
-    cin >> creditos >> semestre >> ano >> nota1 >> nota2;
-    calcularMedia();
-}
 
 class Tipo3 : public Disciplina
 {
@@ -72,36 +59,41 @@ class Tipo3 : public Disciplina
         int codigo, creditos, semestre, ano;
         double notaMonografia, notaApresentacao, notaPratica, media;
     public:
-        void ler();
-        void calcularMedia()    { media = (2*notaMonografia  + 3*notaApresentacao + notaPratica)/6; }
-        string getTipo()        { return "Tipo3";                                                   }
-        int getCodigo()         { return codigo;                                                    }
-        string getNome()        { return nome;                                                      }
-        int getCreditos()       { return creditos;                                                  }
-        double getMedia()       { return media;                                                     }
-        double getNota()        { return notaApresentacao;                                          }
+        void ler() ;
+        void calcularMedia()                 { media = (2*notaMonografia  + 3*notaApresentacao + notaPratica)/6; }
+        string getTipo()                     { return "Tipo3";                                                   }
+        int getCodigo()                      { return codigo;                                                    }
+        string getNome()                     { return nome;                                                      }
+        int getCreditos()                    { return creditos;                                                  }
+        double getMedia()                    { return media;                                                     }
+        double getNotaApresentacao()         { return notaApresentacao;                                          }
 };
 
 void Tipo3::ler()
 {
-    cin >> codigo; cin.ignore();
-    getline(cin, nome);
-    cin >> creditos >> semestre >> ano >> notaMonografia >> notaApresentacao >> notaPratica;
-    calcularMedia();
+    cout << "Codigo -> "; cin >> codigo;
+    cout << "Nome -> "; cin.ignore(); getline(cin, nome);
+    cout << "Creditos -> "; cin >> codigo;
+    cout << "Semestre -> "; cin >> semestre;
+    cout << "Ano -> "; cin >> ano;
+    cout << "Nota da Monografia -> "; cin >> notaMonografia;
+    cout << "Nota da Apresentacao -> "; cin >> notaApresentacao;
+    cout << "Nota Pratica -> "; cin >> notaPratica;
 }
 
 Disciplina* criarDisciplina()
 {
-    int tp;
+    int tipo;
     do
     {
-        cout << "1 - Tipo 1" << endl <<
+        cout << "Selecione o tipo de disciplina:" << endl <<
+                "1 - Tipo 1" << endl <<
                 "2 - Tipo 2" << endl <<
                 "3 - Tipo 3" << endl <<
                 "Entre com a escolha: ";
-        cin >> tp;
-    } while ( tp < 1 || tp > 3);
-    switch (tp)
+        cin >> tipo;
+    } while ( tipo < 1 || tipo > 3);
+    switch (tipo)
     {
         case 1: return new Tipo1();
         case 2: return new Tipo2();
@@ -153,6 +145,7 @@ void Historico::inserir()
 {
     v[qtd] = criarDisciplina();
     v[qtd]->ler();
+    v[qtd]->calcularMedia();
     qtd++;
 }
 
@@ -183,8 +176,8 @@ void Historico::listarDisciplinas()
     for ( int i = 0 ; i < qtd ; i++ )
     {
 
-        cout << v[i]->getNome() << " " << v[i]->getCreditos()
-        << " " << v[i]->getMedia() << " " << v[i]->getTipo() << endl;
+        cout << v[i]->getNome() << " - " << v[i]->getCreditos()
+        << " - " << v[i]->getMedia() << " - " << v[i]->getTipo() << endl;
     }
 }
 
@@ -193,8 +186,8 @@ void Historico::listarDisciplinasTipo2()
     for ( int i = 0 ; i < qtd ; i++ )
     {
         if ( v[i]->getTipo() == "Tipo2" )
-            cout << v[i]->getNome() << " " << v[i]->getCreditos()
-            << " " << v[i]->getMedia() << endl;
+            cout << v[i]->getNome() << " - " << v[i]->getCreditos()
+            << " - " << v[i]->getMedia() << endl;
     }
 }
 
@@ -202,23 +195,23 @@ void Historico::listarDisciplinasTipo3()
 {
     for ( int i = 0 ; i < qtd ; i++ )
     {
-        if ( v[i]->getTipo() == "Tipo3" && v[i]->getNota() > 7.0 )
-            cout << v[i]->getNome() << " " << v[i]->getCreditos()
-            << " " << v[i]->getMedia() << endl;
+        if ( v[i]->getTipo() == "Tipo3" && v[i]->getNotaApresentacao() > 7.0 )
+            cout << v[i]->getNome() << " - " << v[i]->getCreditos()
+            << " - " << v[i]->getMedia() << endl;
     }
 }
 
 int menu()
 {
     int opcao;
-    cout << "bem vindo ao sistema academico" << endl
-    << "1. inserir disciplina" << endl
-    << "2. remover disciplina" << endl
-    << "3. alterar disciplina" << endl
-    << "4. listar todas as disciplinas" << endl 
-    << "5. listar todas as disciplinas de tipo 2" << endl
-    << "6. listar todas as disciplinas de tipo 3 com nota de apresentacao maior que 7" << endl
-    << "entre com sua opcao: ";
+    cout << "Bem vindo ao Sistema Academico" << endl
+    << "1. Inserir disciplina" << endl
+    << "2. Remover disciplina" << endl
+    << "3. Alterar disciplina" << endl
+    << "4. Listar todas as disciplinas" << endl 
+    << "5. Listar todas as disciplinas de tipo 2" << endl
+    << "6. Listar todas as disciplinas de tipo 3 com nota de apresentacao maior que 7" << endl
+    << "Entre com sua opcao: ";
     cin >> opcao;
     return opcao;
 }
